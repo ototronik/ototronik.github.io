cd1a29f6fbeb2a5a194c3aa566b27f373142be98
isasaid <isa.muhammad.said@gmail.com>
2015-10-09 23:51:47 -0700

    ototronik

---
+++ e-learning.php
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+if (window.location.protocol != "https:")
+    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  if( window.location.protocol == 'http://isasaid.github.io/ototronik/comment.html' ) return;
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+var strlike= '<div class="fb-comments" data-href="' + window.location.href + '" data-num-posts="2" data-width="%WIDTH%"></div>';
+document.write( window.location.protocol == 'http://isasaid.github.io/ototronik/comment.html' ? "<img src='assets/fbcomment.png' onclick=\"alert('Content must be hosted on a Web Server to use this feature');\" />" : strlike );
+</script>
